cc9a0c969b7c18b12bdb64f122de6694529f5c5d
Unai Iglesias <unaiiglesiasrodriguez@gmail.com>
2023-08-21 19:17:40 +0200

    Swap to portablemc 4.1 (Forge, no need to run the forge installer manually any longer)

--- src/launch_manager.py
+++ src/launch_manager.py
@@ -1,13 +1,5 @@
-from time import sleep
 from portablemc.standard import Context, Version
 from portablemc.forge import ForgeVersion
-import json
-from os import path, popen, remove
-import wget
-import keyboard
-from pyperclip import copy
-import customtkinter as ctk
-import pygetwindow as gw
 from pathlib import Path
 
 
@@ -23,6 +15,36 @@ def launch_vanilla(launch_parameters, app):
 
     print("Downloading and installing Minecraft version")
     app.update_status("working", app.translations["status_working_downloading_version"])
+    version.set_auth_offline(username, None)  # (username, uuid) no uuid so pass None
+    env = version.install()
+
+    env.resolution = (1080, 720)
+    env.jvm_args.append(f"-Xmx{ram_amount}M")
+
+    print("Launching Minecraft")
+    app.update_status("success", app.translations["status_success"])
+    env.run()
+
+
+def launch_forge(launch_parameters, app):
+    main_dir = Path(launch_parameters["path"])
+    work_dir = Path(main_dir)
+    version_id = launch_parameters["version"]
+    subversion_id = launch_parameters["subversion"]
+    ram_amount = launch_parameters["ram"]
+    username = launch_parameters["username"]
+
+    ctx = Context(main_dir, work_dir)
+
+    full_version_id = f"{version_id}-{subversion_id}"
+    print(f"Launching : {full_version_id}")
+
+    print("Downloading and installing Forge version")
+    app.update_status("working", app.translations["status_working_downloading_version"])
+    version = ForgeVersion(full_version_id, context=ctx)
+
+    version.set_auth_offline(username, None)  # (username, uuid) no uuid so pass None
+
     env = version.install()
 
     env.username = username
@@ -32,206 +54,6 @@ def launch_vanilla(launch_parameters, app):
     print("Launching Minecraft")
     app.update_status("success", app.translations["status_success"])
     env.run()
-
-
-def make_launcher_profiles_json(main_dir):
-    LAUNCHER_PROFILES_BASE = {"profiles": {"(Default)": {"created": "1970-01-01T00:00:00.0000000Z",
-                                                         "javaArgs": "-Xmx5G -XX:+UnlockExperimentalVMOptions"
-                                                                     " -XX:+UseG1GC -XX:G1NewSizePercent\u003d20"
-                                                                     " -XX:G1ReservePercent\u003d20"
-                                                                     " -XX:MaxGCPauseMillis\u003d50"
-                                                                     " -XX:G1HeapRegionSize\u003d32M",
-                                                         "lastUsed": "1970-01-01T00:00:00.0000000Z",
-                                                         "lastVersionId": "latest-release",
-                                                         "name": "",
-                                                         "type": "custom"}},
-                              "settings": {"crashAssistance": True,
-                                           "enableAdvanced": True,
-                                           "enableAnalytics": True,
-                                           "enableHistorical": False,
-                                           "enableReleases": True,
-                                           "enableSnapshots": False,
-                                           "keepLauncherOpen": False,
-                                           "profileSorting": "ByLastPlayed",
-                                           "showGameLog": False,
-                                           "showMenu": False,
-                                           "soundOn": False}}
-
-    with open(main_dir + "\\launcher_profiles.json", "w") as json_file:
-        json.dump(LAUNCHER_PROFILES_BASE, json_file)
-
-    return
-
-
-def download_forge_installer(version, subversion, main_dir):
-
-    """
-    It turns out that the maven of all forge installers is public, there is no avaliable documentation but files can be
-    downloaded from it. Using this "template" / "example" the url to the installer can be guessed without web
-    scraping it.
-
-    ex: https://maven.minecraftforge.net/net/minecraftforge/forge/1.16.2-33.0.58/forge-1.16.2-33.0.58-installer.jar
-    """
-
-    full_version = f"{version}-{subversion}"
-
-    needed_url = f"https://maven.minecraftforge.net/net/minecraftforge/forge/{full_version}/forge-{full_version}-installer.jar"
-    print(needed_url)
-
-    # Download the installer into the main_dir (it will be named installer.jar)
-    print("Downloading forge installer")
-    wget.download(needed_url, f"{main_dir}\\installer.jar")
-
-
-def automatically_launch_forge_installer(installer_path, main_dir, version, app):
-    sleep(1)
-    popen(installer_path, "r", 1)  # Open the forge installer
-
-    forge_installer_windows = []
-    while len(forge_installer_windows) == 0:  # If the list's length is 0, no window was found
-        print("Looking for Forge installer window...")
-        app.update_status("error", app.translations["status_error_installer_not_found"])
-        forge_installer_windows = gw.getWindowsWithTitle('Mod system installer')
-        sleep(1)
-    print("Forge installer window found!")
-
-    app.update_status("working", app.translations["status_working_setting_forge_installer"])
-
-    # In order to ensure that the forge installer is focused, the window will be "activated" (focused)
-    forge_installer_window = gw.getWindowsWithTitle('Mod system installer')[0]
-    forge_installer_window.activate()
-    print("Forge installer window focused")
-
-    copy(main_dir)
-    print("main_dir copied to clipboard")
-
-    if version in ("1.7.2", "1.7.10", "1.8", "1.8.", "1.8.9", "1.9", "1.9.4", "1.10", "1.10.2",
-                   "1.11.2", "1.14.2", "1.14.3", "1.15", "1.15.1", "1.16.1", "1.16.2", "1.16.3", "1.16.4",
-                   "1.16.5", "1.17.1", "1.18", "1.18.1", "1.18.2", "1.19", "1.19.1", "1.19.2", "1.19.3",
-                   "1.19.4", "1.20", "1.20.1"):  # These need an extra tab
-        keyboard.send("tab")
-        sleep(0.1)
-    elif version in ("1.11", "1.12", "1.12.1"):  # These need 2 extra tabs
-        keyboard.send("tab")
-        sleep(0.1)
-        keyboard.send("tab")
-        sleep(0.1)
-    elif version in ("1.12.2", "1.13.2", "1.14.4", "1.15.2"):  # These don't need extra tabs
-        pass
-    else:  # Looks like most versions need an extra tab, so....
-        keyboard.send("tab")
-        sleep(0.1)
-
-    keyboard.send("tab")
-    sleep(0.1)
-    keyboard.send("tab")
-    sleep(0.1)
-    keyboard.send("tab")
-    sleep(0.1)
-    keyboard.send("tab")
-    sleep(0.1)
-
-    keyboard.send("enter")
-    sleep(0.3)
-    keyboard.send("control+v")
-    sleep(0.3)
-    keyboard.send("enter")
-    sleep(0.3)
-    keyboard.send("tab")
-    sleep(0.1)
-    keyboard.send("enter")
-    print("Forge installer set up and running")
-    app.update_status("working", app.translations["status_working_forge_installer_running"])
-
-
-def ask_if_forge_installation_has_finished(installer_path, app):
-    """
-    Since it would be hard to check whether the forge installation has finished,
-    this function will pop up a window to ask the user before continuing with the forge installation. 
-    """
-
-    class AskIfForgeHasFinishedPopUp(ctk.CTkToplevel):
-        def __init__(self, master):
-            super().__init__()
-            self.title("Ha acabado de instalarse Forge?")
-            self.geometry("600x200")
-
-            self.text = ctk.CTkTextbox(self, width=560, height=80)
-            self.text.insert("1.0", "Espera hasta que forge termine de instalarse.\n")
-            self.text.insert("2.0", "Una vez termine, pulsa OK para que se cierre la ventana de instalación.\n")
-            self.text.insert("3.0", "Entonces, presiona el botón de abajo.")
-            self.text.pack(padx=20, pady=10)
-
-            self.button = ctk.CTkButton(self, text="Forge ha acabado de instalarse", command=self.destroy, width=300,
-                                        height=60)
-            self.button.pack(padx=50, pady=10)
-
-            self.transient(master)  # The pop-up will stay on top of the launcher window
-            self.grab_set()  # Hijack the launcher window so that it can't be clicked
-            master.wait_window(self)  # Tell the launcher window to wait (block itself)
-
-    pop_up = AskIfForgeHasFinishedPopUp(app)
-    print("Forge installation finished")
-    sleep(1)  # Give the forge installer some time to completely close, in case the user clicks too fast
-    remove(installer_path)  # Remove installer as it is no longer needed
-    print("Removing used installer")
-
-
-def launch_forge(launch_parameters, app):
-    main_dir = launch_parameters["path"]
-    work_dir = main_dir
-    version_id = launch_parameters["version"]
-    subversion_id = launch_parameters["subversion"]
-    ram_amount = launch_parameters["ram"]
-    username = launch_parameters["username"]
-
-    screen_resolution = (app.winfo_screenwidth(), app.winfo_screenheight())
-
-    if not path.isfile(main_dir + "\\launcher_profiles.json"):
-        app.update_status("working", app.translations["status_working_making_launcher_profiles"])
-        make_launcher_profiles_json(main_dir)
-    #  If there is no launcher_profiles json, make one. The forge installer will need it to run.
-
-    ctx = Context(main_dir, work_dir)
-
-    full_version_id = f"{version_id}-{subversion_id}"
-    print(f"Launching : {full_version_id}")
-
-    installer = ForgeVersionInstaller(ctx, full_version_id)
-
-    if installer.needed():  # The version needs to be installed
-
-        # Download and use forge manual version installer
-        app.update_status("working", app.translations["status_downloading_forge_installer"])
-        download_forge_installer(version_id, subversion_id, main_dir)
-
-        app.update_status("working", app.translations["status_working_forge_installer_working"])
-        installer_path = f"{main_dir}\\installer.jar"
-        automatically_launch_forge_installer(installer_path, main_dir, version_id, app)
-        ask_if_forge_installation_has_finished(installer_path, app)
-
-        print("Downloading and installing Forge version")
-        app.update_status("working", app.translations["status_working_downloading_forge_version"])
-        installer.prepare()
-        installer.download()
-        installer.install()
-
-    print("Downloading and installing Minecraft version")
-    app.update_status("working", app.translations["status_working_dowloading_version"])
-    version = ForgeVersion(ctx, full_version_id)
-    version.install(jvm=True)
-
-    start_opts = StartOptions()
-    start_opts.username = username
-    start_opts.disable_multiplayer = False
-    start_opts.resolution = (1080, 720)
-
-    print("Launching Minecraft")
-    app.update_status("success", app.translations["status_success"])
-    start = Start(version)
-    start.prepare(start_opts)
-    start.jvm_args.append(f"-Xmx{ram_amount}M")
-    start.start()
 
 
 if __name__ == "__main__":
